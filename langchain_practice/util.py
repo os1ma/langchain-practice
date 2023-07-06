af3a00892c4893a7d0a7dd261398b0f0bf8f736c
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-07-06 12:01:49 +0900

    Add multi_query_retriever.py

--- langchain_practice/util.py
+++ langchain_practice/util.py
@@ -1,3 +1,5 @@
+import json
+
 import langchain
 import openai
 from dotenv import load_dotenv
@@ -7,3 +9,11 @@ def initialize():
     load_dotenv()
     langchain.verbose = True
     openai.log = "debug"
+
+
+def pretty_print_docs(docs):
+    print(
+        f"\n{'-' * 100}\n".join(
+            [f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]
+        )
+    )
